eba290d21f05f6f074a455cb57b888c3eabae4f3
ssokhnadiop-ai <ssokhnadiop@icloud.com>
2025-11-21 15:50:25 +0100

    Create tictactoe.py

---
+++ tictactoe.py
@@ -0,0 +1,15 @@
+import tkinter as tk
+from tkinter import messagebox
+
+window = tk.Tk()
+window.title("TIC-TAC-TOE")
+
+digits = list(range(1, 10))
+
+mark = ''
+
+count = 0
+
+panels = ['panel'] + list(range(1, 10))
+
+
